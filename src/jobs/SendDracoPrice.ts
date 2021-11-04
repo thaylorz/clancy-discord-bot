27ea48317b0156eb81b3e992f6422b34345b522b
Thaylor Santos <thaylormichi@gmail.com>
2021-11-04 19:58:02 -0300

    price formation

--- src/jobs/SendDracoPrice.ts
+++ src/jobs/SendDracoPrice.ts
@@ -18,20 +18,15 @@ function sendDracoPrice() {
                 .setAuthor('mir4 draco', '', 'https://www.mir4draco.com')
                 .setFields({
                     name: 'USD/DRACO',
-                    value: `${parseFloat(dracoData.USDDracoRate).toLocaleString('pt-br', { style: 'currency', currency: 'USD', minimumFractionDigits: 4 })}\r
-                    ${dracoData.USDDracoVariation.toLocaleString('pt-br', { minimumFractionDigits: 2 })}%`,
+                    value: `${parseFloat(dracoData.USDDracoRate).toLocaleString('pt-br', { style: 'currency', currency: 'USD', minimumFractionDigits: 4 })}\r${dracoData.USDDracoVariation.toLocaleString('pt-br', { minimumFractionDigits: 2 })}%`,
                     inline: true,
                 }, {
                     name: 'WEMIX/DRACO',
-                    value: `
-                        ${parseFloat(dracoData.DracoPriceWemix).toLocaleString('pt-br', { style: 'currency', currency: 'USD', minimumFractionDigits: 4 })}\r
-                        ${dracoData.USDWemixVariation.toLocaleString('pt-br', { minimumFractionDigits: 2 })}`,
+                    value: `${parseFloat(dracoData.DracoPriceWemix).toLocaleString('pt-br', { style: 'currency', currency: 'USD', minimumFractionDigits: 4 })}\r${dracoData.USDWemixVariation.toLocaleString('pt-br', { minimumFractionDigits: 2 })}`,
                     inline: true,
                 }, {
                     name: 'BRL/DRACO',
-                    value: `
-                        ${dracoData.BRLDracoRate.toLocaleString('pt-br', { style: 'currency', currency: 'BRL', minimumFractionDigits: 4 })}\r
-                        ${dracoData.BRLDracoVariation.toLocaleString('pt-br', { minimumFractionDigits: 2 })}`,
+                    value: `${dracoData.BRLDracoRate.toLocaleString('pt-br', { style: 'currency', currency: 'BRL', minimumFractionDigits: 4 })}\r${dracoData.BRLDracoVariation.toLocaleString('pt-br', { minimumFractionDigits: 2 })}`,
                     inline: true,
                 })
                 .setImage('https://file.mir4global.com/draco/img/mir4draco.png')
